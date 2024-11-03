f0b77cdd8da59d265d33768a8fe366d8f950413e
midnight <ivanator666@gmail.com>
2024-11-03 23:05:20 +0300

    updated bot

--- saverbot/bot.py
+++ saverbot/bot.py
@@ -1,19 +1,59 @@
 import os
-import asyncio
-import telegram
+import logging
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import (
+        filters,
+        ApplicationBuilder,
+        ContextTypes,
+        CommandHandler,
+        MessageHandler
+        )
 
+from parser import parser
+
+
+logging.basicConfig(
+        level=logging.INFO,
+        filemode=f'{__name__}.log',
+        format='%(asctime)s %(levelname)s %(message)s',
+        )
 
 load_dotenv()
-bot_token = os.getenv('BOT_TOKEN')
-print(bot_token)
+token = os.getenv('BOT_TOKEN')
 
 
-async def main():
-    bot = telegram.Bot(bot_token)
-    async with bot:
-        print(await bot.get_me())
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = '''
+    Hi! I can dowload any media from Instagram, Tiktok and Pintrest by link.
+    I also can download whole profiles but require $$motivation$$
+    '''
+    await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text
+            )
+
+
+async def process_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = parser.parse_url(update.message.text)
+    await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text if text else update.message.text
+            )
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(token).build()
+
+    start_handler = CommandHandler(
+            'start',
+            start
+            )
+    echo_handler = MessageHandler(
+            filters.TEXT & (~filters.COMMAND),
+            process_text
+            )
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.run_polling()

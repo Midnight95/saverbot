20ea40d066fce5ae9dbce3dfc334a31b342b181a
midnight95 <ivanator666@gmail.com>
2024-11-05 22:59:34 +0300

    updated messages

--- saverbot/bot.py
+++ saverbot/bot.py
@@ -34,9 +34,10 @@ Tiktok and Pintrest by link."""
 
 async def process_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = parser.parse_url(update.message.text)
+    message = 'I require URL'
     await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=text if text else update.message.text
+            text=f'this is {text} url' if text else message
             )
 
 
@@ -46,13 +47,13 @@ def main():
             'start',
             start
             )
-    echo_handler = MessageHandler(
+    url_handler = MessageHandler(
             filters.TEXT & (~filters.COMMAND),
             process_text
             )
 
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(url_handler)
     application.run_polling()
 
 

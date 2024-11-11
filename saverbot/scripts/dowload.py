0f794f54b65c95cebb548eba19dbbe0465046ee7
midnight95 <ivanator666@gmail.com>
2024-11-12 00:23:56 +0300

    wasted 4 hours to get filename from yt-dlp

--- saverbot/scripts/dowload.py
+++ saverbot/scripts/dowload.py
@@ -10,4 +10,7 @@ OPTS = {
 
 def download(url: str):
     with YoutubeDL(OPTS) as loader:
+        info = loader.extract_info(url, download=False)
+        fn = loader.prepare_filename(info)
         loader.download(url)
+        return fn

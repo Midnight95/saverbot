fc85cad7d2cd4f06a9ad9977ac4ab11591e49bd8
midnight95 <ivanator666@gmail.com>
2024-11-15 00:13:02 +0300

    working on separate logic for instagram

--- saverbot/scripts/dowload.py
+++ saverbot/scripts/dowload.py
@@ -1,4 +1,8 @@
 from yt_dlp import YoutubeDL
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 
 OPTS = {
@@ -8,9 +12,18 @@ OPTS = {
         'verbose': True,
         }
 
+INSTAGRAM_CREDENTIALS = {
+        'username': os.getenv('INSTAGRAM_USERNAME'),
+        'password': os.getenv('INSTAGRAM_PASSWORD')
+        }
+
 
 async def download(url: str) -> str:
     with YoutubeDL(OPTS) as loader:
         info_dict = loader.extract_info(url, download=True)
         file_name = loader.prepare_filename(info_dict)
         return file_name
+
+
+def instagram_auth(username, password):
+    pass
